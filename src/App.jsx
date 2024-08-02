cc56a926b00295ed64c5390f1291862d05a56516
YasSer <51763622+YasSerhbh@users.noreply.github.com>
2024-08-02 17:51:57 +0100

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import About from './components/About'
+import Community from './components/Community'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import Hero from './components/Hero'
+
+function App() {
+  return (
+    <div>
+        <Header />
+        <Hero />
+        <About />
+        <Community />
+        <Footer />
+    </div>
+  )
+}
+
+export default App
